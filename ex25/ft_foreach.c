b476783a36b377f434d9ab569bb7ceb701e0516d
oruban <oruban@student.42wolfsburg.de>
2023-11-11 18:40:26 +0100

    till ex26 exc

--- ex25/ft_foreach.c
+++ ex25/ft_foreach.c
@@ -6,7 +6,7 @@
 /*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 15:01:36 by oruban            #+#    #+#             */
-/*   Updated: 2023/11/11 15:51:06 by oruban           ###   ########.fr       */
+/*   Updated: 2023/11/11 18:16:13 by oruban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,51 @@
 // to display all ints of the array :
 // ft_foreach(tab, 1337, &ft_putnbr);
 
-void	ft_putnbr(int nb);
+#include <unistd.h>
+
+void	ft_putnbr(int nb)
+{
+	char	output;
+
+	if (nb == 0)
+	{
+		write(1, "0", 1);
+		return ;
+	}
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+	}
+	output = nb % 10 + '0';
+	write(1, &output, 1);
+}
 
 void ft_foreach(int *tab, int length, void (*f)(int))
 {
-
+	int i;
+	
+	i = 0;
+	while (i < length)
+	{
+		f(tab[i]);
+		i++;
+	}
 }
 
 int main(void)
 {
-	int *tab = { -2, -1, 0, 1, 2};
-	int length = 5;
-	void (*f)(int);
+	int tab[] = {-2, -1, 0, 1, 2, 3, 4, 5};
+	int length = 7;
 	
-	
-
-	
+	ft_foreach(tab, length, ft_putnbr);
 }
---
+++ ex26/ft_count_if.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_if.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 18:23:05 by oruban            #+#    #+#             */
+/*   Updated: 2023/11/11 18:25:13 by oruban           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// • Create a function ft_count_if which will return the number of elements 
+// of the array that return 1, passed to the function f.
+// • The array will be delimited by 0.
+
+int ft_count_if(char **tab, int (*f)(char*))
+{
+	
+}
